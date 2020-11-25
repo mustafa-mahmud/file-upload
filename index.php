0959841f8fcdb057aed0f97855a9add69cae8cd7
mithu <mithu@gmail.com>
2020-11-25 13:54:43 +0600

    fix issue

--- index.php
+++ index.php
@@ -1,46 +1,10 @@
 <?php
+
+include 'upload.php';
+
 if (isset($_POST['submit'])) {
   $user = $_POST['user'];
   $file = $_FILES['photo'];
-
-  function fileUpload($file) {
-    global $user;
-
-    $name = $file['name'];
-    $type = strtolower(substr($file['type'], 6));
-    $temp = $file['tmp_name'];
-    $size = $file['size'];
-
-    $acceptType = ['jpg', 'jpeg', 'png', 'gif'];
-    $acceptSize = 1000000; //1mb
-
-    if (in_array($type, $acceptType)) {
-      if (!($size > $acceptSize)) {
-        $folder = 'uploads/';
-        $name = intval(microtime(true)) . rand() . $name;
-
-        $success = move_uploaded_file($temp, $folder . $name);
-
-        if ($success) {
-          message('alert-success', ucfirst($user) . ' your file uploaded successfully.');
-        } else {
-          message('alert-danger', ucfirst($user) . ' something went wrong pls try again.');
-        }
-      } else {
-        $mb = is_float($size / $acceptSize) ? number_format(($size / $acceptSize), 2) : ($size / $acceptSize);
-        message('alert-danger', 'File size must be in 1mp but your one is ' . $mb . 'mb');
-      }
-    } else {
-      message('alert-danger', 'Only jpg or jpeg or png or gif supported');
-    }
-  }
-
-  function message($color, $msg) {
-    echo "<div class='m-2 alert $color alert-dismissible fade show text-center' role='alert'>
-	$msg
-		<button type='button' class='btn-close' data-dismiss='alert' aria-label='Close'></button>
-	</div>";
-  }
 
   if (trim($user) === '') {
     message('alert-danger', 'Pls put user name.');
@@ -49,8 +13,8 @@ if (isset($_POST['submit'])) {
       fileUpload($file);
     }
   }
-
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
